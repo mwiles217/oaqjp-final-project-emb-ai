cb95c369f3841bf948070d5bf95739092eb67900
Michael Wiles <mwiles217@gmail.com>
2025-04-19 14:58:10 -0500

    final changes with submissions

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,11 +1,20 @@
 import requests,json  # Import the requests library to handle HTTP requests
-
 def emotion_detector(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)    
     url= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj= { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     formatted_response=json.loads(response.text)
+    if response.status_code ==500 or response.status_code==400:
+        return {
+        "anger": None,
+        "disgust": None,
+        "fear": None,
+        "joy": None,
+        "sadness": None,
+        "dominant_emotion": None
+        }
+
     em=formatted_response["emotionPredictions"][0]["emotion"]
     dom:str=""
     dv:float=0
@@ -17,29 +26,10 @@ def emotion_detector(text_to_analyse):  # Define a function named sentiment_anal
         if float(v)>float(dv):
             dom=k
             dv=float(v)
-    # print(dom)
     em["dominant_emotion"]=dom
 
-    #return em
-    # fj=json.dumps(em,indent=4)
-    # print(fj)
+    return em
 
-    #print(em)
-        
-    #print(formatted_response["emotionPredictions"][0]["emotion"].keys())
-    # return formatted_response
-
-    if response.status_code == 200:
-        return em
-    elif response.status_code ==500 or response.status_code==400:
-        return {
-        "anger": None,
-        "disgust": None,
-        "fear": None,
-        "joy": None,
-        "sadness": None,
-        "dominant_emotion": None
-        }
     # return response.text  # Return the response text from the API
 
 
--- server.py
+++ server.py
@@ -1,36 +1,33 @@
-''' Executing this function initiates the application of sentiment
+""" 
+    Executing this function initiates the application of sentiment
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
-'''
-from flask import Flask, render_template, request #from the flask pramework package : TODO
+"""
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-
-#Initiate the flask app : TODO
 app=Flask(__name__)
-
 @app.route("/emotionDetector")
-def sent_analyzer():
-    # Retrieve the text to analyze from the request arguments
+def emotion_analyzer():
+    """
+        Emotion Analyzer texts text as input and finds scored for a range of emotions
+        anger,disgust,fear,sadness 
+        and returns the dominant emotion
+    """
     text_to_analyze = request.args.get("textToAnalyze")
-
-    # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
+    if response['anger'] is None:
+        return "Invalid text! Please try again!"
 
-    # Extract the label and score from the response
-    label = response['label']
-    score = response['score']
-
-    if label is None:
-        return "Invalid text! Please Try Again!"
-    else:
-        # Return a formatted string with the sentiment label and score
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    fs=f"For the given statement, the system response is 'anger':  {response['anger']}"  + \
+    f" 'disgust': {response['disgust']}, 'fear': {response['fear']}" + \
+    f", 'joy': {response['joy']} and 'sadness': {response['sadness']}." + \
+    f" The dominant emotion is <b>{response['dominant_emotion']}</b>."
+    return fs
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
     return render_template("index.html")
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
