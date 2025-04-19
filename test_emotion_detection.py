a527c911c7b6f7ac77def012d770e8515b84add1
Michael Wiles <mwiles217@gmail.com>
2025-04-19 12:12:15 -0500

    initial save

---
+++ test_emotion_detection.py
@@ -0,0 +1,35 @@
+from EmotionDetection.emotion_detection import emotion_detector
+import unittest
+# v=sentiment_analyzer("I Love Tech")
+# print(v)
+
+class TestSentimentAnalyzer(unittest.TestCase):
+    def test_sentiment_analyzer(self):
+        # Test case for joy 
+        result_1 = emotion_detector('I am glad this happened') #joy
+        self.assertEqual(result_1['label'], 'SENT_POSITIVE')
+        
+        # Test case for anger 
+        result_2 = emotion_detector('I am really mad about this') #anger
+        self.assertEqual(result_2['label'], 'SENT_NEGATIVE')
+        
+        # Test case for disgust 
+        result_3 = emotion_detector('I feel disgusted just hearing about this') #disgust
+        self.assertEqual(result_3['label'], 'SENT_NEUTRAL')
+
+         # Test case for sadness
+        result_4 = emotion_detector('I am so sad about this') #sadness
+        self.assertEqual(result_4['label'], 'SENT_NEUTRAL')
+
+
+        # Test case for fear
+        result_5 = emotion_detector('I am really afraid that this will happen') #fear
+        self.assertEqual(result_5['label'], 'SENT_NEUTRAL')
+
+
+unittest.main()
+
+
+
+
+# I am really afraid that this will happen	fear
